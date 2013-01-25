99931132eda933d77405bb1a5a82f49d7519ec64
David Lancea <dlancea@gmail.com>
2013-01-25 12:13:14 -0500

    Update DependencyInjection/DlanceaCoffeePhpExtension.php
    Turns out you shouldn't just delete random files.

--- DependencyInjection/DlanceaCoffeePhpExtension.php
+++ DependencyInjection/DlanceaCoffeePhpExtension.php
@@ -18,9 +18,6 @@ class DlanceaCoffeePhpExtension extends Extension {
 	 * {@inheritDoc}
 	 */
 	public function load(array $configs, ContainerBuilder $container) {
-		$configuration = new Configuration();
-		$config = $this->processConfiguration($configuration, $configs);
-		
 		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 		$loader->load('services.yml');
 	}
